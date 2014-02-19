5d7d1301677683ef098a9811f5ac37a17463e0cc
Guiem <g@guiem.info>
2014-02-19 11:35:46 +0100

    order.py

--- shop/models/defaults/order.py
+++ shop/models/defaults/order.py
@@ -2,10 +2,22 @@
 from django.utils.translation import ugettext_lazy as _
 from shop.models_bases import BaseOrder
 from shop.models_bases.managers import OrderManager
-
+from myproject.addressmodel import Address
+from django.db import models
 
 class Order(BaseOrder):
     objects = OrderManager()
+
+    shipping_address = models.ForeignKey(Address, verbose_name=_('Dirección de envío'), blank=True, null=True, related_name='shipping_address')
+    billing_address = models.ForeignKey(Address, verbose_name=_('Dirección de facturación'), blank=True, null=True, related_name='billing_address')
+
+    def set_billing_address(self, billing_address):
+        self.billing_address = billing_address
+        self.save()
+
+    def set_shipping_address(self, shipping_address):
+        self.shipping_address = shipping_address
+        self.save()
 
     class Meta(object):
         abstract = False
